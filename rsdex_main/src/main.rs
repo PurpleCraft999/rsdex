7df18ffac1d00cf7bf83664df7e7a3392cf7d4ad
PurpleCraft999 <152620081+PurpleCraft999@users.noreply.github.com>
2026-02-19 19:45:04 -0500

    new version

--- rsdex_main/src/main.rs
+++ rsdex_main/src/main.rs
@@ -17,7 +17,6 @@ fn main() {
     };
 
     if args.help {
-        // println!("{}",READ_ME);
         termimad::print_text(READ_ME);
 
         return;
@@ -40,46 +39,15 @@ fn main() {
     }
 }
 
-///rsdex is a cli that allow you to locally search for pokemon like the pokedex would allow you to.
-///
-///a thanks to PokéApi for the data
 #[derive(clap::Parser)]
 #[command(version, disable_help_flag = true)]
 struct Args {
-    ///takes a pokemon's name,color,type,stat,egg group or dex number
-    ///
-    /// to get dex number a simple number will work
-    ///
-    /// for name entering any pokemon name will work
-    ///
-    /// for species with a space in their name use a `-`
-    ///
-    /// for color typing a pokemons color will work
-    ///
-    /// ***Stats***
-    ///
-    /// to search a stat you append the stat you want to a number at the end
-    /// also you can put a `l` or `g` in front of the number to get pokemon with stats that are ≤ or ≥
-    /// # Examples
-    ///
-    /// `rsdex 25hp` for pokemon with 25 hp exactly
-    ///
-    /// `rsdex 10sa` for pokemon with 10 special attack exactly
-    ///
-    /// `rsdex l120d` for pokemon with ≤ 120 defence
-    ///
-    /// `rsdex g110s` for pokemon with ≥ 110 speed
-    ///
+
     #[arg(value_parser = SearchQuery::parser)]
     search_queries: Vec<SearchQuery>,
-    // #[arg(value_parser = SearchValue::parser)]
-    // /// if you have this you can search for pokemon meeting both criteria
-    // /// some options are disabled such as name and dex number because it will always return an error
-    // second_search_value: Option<SearchValue>,
-    ///provides more detail the higher the number given
+
     #[arg(long, short,value_parser = value_parser!(u8).range(0..=5),default_value_t=0)]
     detailed: u8,
-    ///will write to the given path with the specified data level in the format specified by write-mode
     #[arg(long, alias("fp"))]
     file_path: Option<PathBuf>,
     #[arg(long, requires = "file_path")]
